205bc0dade2b8e054cc2a2a2cb251175df56d21b
胡琪 <1335326918@qq.com>
2021-06-08 16:43:12 +0800

    前端

--- src/main.js
+++ src/main.js
@@ -1,6 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+
+//导入elementui
+import  'element-ui/lib/theme-chalk/index.css'
+import  ElementUI from 'element-ui'
+
+Vue.use(ElementUI)
+
+//导入axios
+import Axios from 'axios'
+
+//设置baseurl   指定为后端的访问路径
+Axios.defaults.baseURL="http://localhost:8888";
+
+Vue.prototype.$axios = Axios;
+
+
 new Vue({
   el: '#app',
   render: h => h(App)
