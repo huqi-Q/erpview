c7b473dcdf9ff7c653dfe44d159624ec70c1f895
胡琪 <1335326918@qq.com>
2021-06-08 17:01:39 +0800

    前端

---
+++ src/components/view/MenuList.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      菜单管理
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "MenuList"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/view/Welcome.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      欢迎使用本系统
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Welcome"
+    }
+</script>
+
+<style scoped>
+
+</style>
